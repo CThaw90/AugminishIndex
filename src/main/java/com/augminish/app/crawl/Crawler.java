2b8728f758545479155bdfa1ee8c4913cefcddff
CThaw90 <christhaw90@gmail.com>
2016-01-21 17:29:03 -0500

    Developed crawler functionality and applied additional unit tests.

--- src/main/java/com/augminish/app/crawl/Crawler.java
+++ src/main/java/com/augminish/app/crawl/Crawler.java
@@ -1,6 +1,9 @@
 package com.augminish.app.crawl;
 
 import com.augminish.app.common.util.file.FileHandler;
+import com.augminish.app.common.util.mysql.MySQL;
+import com.augminish.app.common.util.mysql.helper.SqlBuilder;
+import com.augminish.app.common.util.object.PropertyHashMap;
 import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
 import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.WebResponse;
@@ -10,6 +13,7 @@ import org.jsoup.nodes.Document;
 
 import java.io.IOException;
 import java.net.MalformedURLException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
@@ -17,10 +21,14 @@ import java.util.Queue;
 
 public class Crawler extends Thread {
 
+    private static final String WebSitesTable = "`SearchIndex`.WebSites";
+
     private HashMap<String, String> visited;
     private Queue<String> queue;
 
+    private PropertyHashMap propertyHashMap;
     private FileHandler filehandler;
+    private MySQL mysql;
 
     private WebResponse response;
     private WebClient webclient;
@@ -30,19 +38,22 @@ public class Crawler extends Thread {
     public Crawler(List<String> seeds) {
 
         webclient = new WebClient(com.gargoylesoftware.htmlunit.BrowserVersion.FIREFOX_38);
+        visited = new HashMap<String, String>();
         queue = new LinkedList<String>(seeds);
         filehandler = new FileHandler();
+        mysql = new MySQL();
+
     }
 
+    /*** Constructor used for JUnit testing purposes ***/
     protected Crawler() {
-
+        visited = new HashMap<String, String>();
     }
 
     @Override
     public void run() {
         try {
             crawl();
-
         }
         catch (FailingHttpStatusCodeException fhse) {
             // TODO: Store Http Status Code Exception for human investigation
@@ -63,7 +74,12 @@ public class Crawler extends Thread {
 
     protected void crawl() throws FailingHttpStatusCodeException, MalformedURLException, IOException {
 
+        propertyHashMap = new PropertyHashMap();
+
         String url, domain, content;
+        loadOutdatedWebSites();
+        loadVisitedWebSites();
+
         while (!queue.isEmpty()) {
 
             response = webclient.getPage(queue.poll()).getWebResponse();
@@ -71,7 +87,7 @@ public class Crawler extends Thread {
             content = response.getContentAsString();
             domain = getDomainFrom(url);
 
-            saveToFile(domain, verifyHash(url), content);
+            save(domain, url, content);
             // TODO: Place this logic in another thread for efficiency
             document = Jsoup.parse(content);
         }
@@ -79,21 +95,91 @@ public class Crawler extends Thread {
         // TODO: [LOGGER] Publicly log Crawler has run out of queues
     }
 
-    protected String verifyHash(String url) {
-        return url;
+    protected String uniqueHash(String hash) {
+
+        int fullLength = hash.length(), offset = 0, start = 0, end = 7;
+        boolean unique = false;
+
+        while (!unique && !(start == 0 && end > fullLength)) {
+
+            unique = !visited.containsKey(hash.substring(start + offset, end + offset));
+            if (!unique) {
+                offset = (offset + end < fullLength ? offset + 1 : 0);
+                end = (end <= fullLength && offset == 0 ? end + 1 : end);
+            }
+        }
+
+        return unique ? hash.substring(start + offset, end + offset) : null;
     }
 
     protected String getDomainFrom(String url) {
         return url.replaceAll("http(s)?://", "").replaceAll("/.*", "");
     }
 
-    private void saveToFile(String domain, String url, String content) throws IOException {
-        String hashedUrl = org.apache.commons.codec.binary.Base64.encodeBase64String(url.getBytes()).substring(0, 7);
+    protected String getURIComponentFrom(String url) {
+        return null;
+    }
+
+    private void save(String domain, String url, String content) throws IOException {
+        String hashedUrl = uniqueHash(org.apache.commons.codec.binary.Base64.encodeBase64String(url.getBytes()));
         if (filehandler.save(domain + "/" + hashedUrl, content)) {
 
         }
         else {
             throw new IOException("Could not save file");
         }
+    }
+
+    private void loadVisitedWebSites() {
+
+        List<HashMap<String, Object>> values = new ArrayList<HashMap<String, Object>>();
+        boolean connected = mysql.isConnected();
+        if (!connected) {
+            connected = mysql.use(propertyHashMap.get("mysql.database"));
+        }
+        if (connected) {
+            values = mysql.select(SqlBuilder.select(WebSitesTable, "domain", "url", "secure", "hash", "needsUpdate").where("needsUpdate == 0").commit());
+            for (HashMap<String, Object> value : values) {
+                visited.put((String) value.get("hash"), buildFullUrl(value));
+            }
+        }
+        else {
+            // TODO: Create a recovery algorithm for a failed MySQL connection
+        }
+    }
+
+    private void loadOutdatedWebSites() {
+
+        List<HashMap<String, Object>> values = new ArrayList<HashMap<String, Object>>();
+        boolean connected = mysql.isConnected();
+        if (!connected) {
+            connected = mysql.use(propertyHashMap.get("mysql.database"));
+        }
+        if (connected) {
+            values = mysql.select(SqlBuilder.select(WebSitesTable, "domain", "url", "secure", "hash", "needsUpdate").where("needsUpdate == 1").commit());
+            for (HashMap<String, Object> value : values) {
+                queue.add(buildFullUrl(value));
+            }
+        }
+        else {
+            // TODO: Create a recovery algorithm for a failed MySQL connection
+        }
+    }
+
+    protected static String buildFullUrl(HashMap<String, Object> webSiteRow) {
+        StringBuilder fullUrl = new StringBuilder();
+        fullUrl.append((Integer) webSiteRow.get("secure") == 1 ? "https://" : "http://");
+        fullUrl.append(webSiteRow.get("domain")).append(webSiteRow.get("url"));
+        return fullUrl.toString();
+    }
+
+    /*** Methods used for JUnit testing purposes ***/
+
+    protected void addVisitedUrl(String hash, String url) {
+        visited.put(hash, url);
+    }
+
+    protected String getVisitedUrl(String key) {
+        return visited.get(key);
     }
 }
--- src/test/java/com/augminish/app/crawl/CrawlerTest.java
+++ src/test/java/com/augminish/app/crawl/CrawlerTest.java
@@ -4,6 +4,8 @@ import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import java.util.HashMap;
+
 public class CrawlerTest {
 
     private static Crawler crawler;
@@ -16,12 +18,45 @@ public class CrawlerTest {
 
     @Test
     public void getDomainFromUrlTest() {
-        String craigslist_url = "https://newjersey.craigslist.org/mus/search/gig73821.html", 
-               amazon_url = "https://www.amazon.com/ecommerce/index.html",
-               regex_url = "www.regular-expressions.info/optional.html";
+        String craigslistUrl = "https://newjersey.craigslist.org/mus/search/gig73821.html", amazonUrl = "https://www.amazon.com/ecommerce/index.html",
+                regexUrl = "www.regular-expressions.info/optional.html", ec2Url = "ec2-54-196-32-193.compute-1.amazonaws.com/projects/lens-platform";
 
-        Assert.assertEquals("Should extract the Amazon Domain name", "www.amazon.com", crawler.getDomainFrom(amazon_url));
-        Assert.assertEquals("Should extract the Craigslist Domain name", "newjersey.craigslist.org", crawler.getDomainFrom(craigslist_url));
-        Assert.assertEquals("Should extract the Regex Domain name", "www.regular-expressions.info", crawler.getDomainFrom(regex_url));
+        Assert.assertEquals("Should extract the Amazon Domain name", "www.amazon.com", crawler.getDomainFrom(amazonUrl));
+        Assert.assertEquals("Should extract the Craigslist Domain name", "newjersey.craigslist.org", crawler.getDomainFrom(craigslistUrl));
+        Assert.assertEquals("Should extract the Regex Domain name", "www.regular-expressions.info", crawler.getDomainFrom(regexUrl));
+        Assert.assertEquals("Should extract the EC2 Instance Domain name", "ec2-54-196-32-193.compute-1.amazonaws.com", crawler.getDomainFrom(ec2Url));
+    }
+
+    @Test
+    public void uniqueHashTest() {
+        String[] hashPermutation = { "1234567", "2345678", "3456789", "4567890", "12345678", "23456789", "34567890", "123456789", "234567890", "1234567890" };
+        String hash = "1234567890";
+
+        for (String uniqueHash : hashPermutation) {
+            Assert.assertEquals("Crawler unique hashing algorithm should match as expected", uniqueHash, crawler.uniqueHash(hash));
+            crawler.addVisitedUrl(uniqueHash, hash);
+        }
+
+        Assert.assertNull("Crawler unique hashing algorithm should return null after running out of hashing schemes", crawler.uniqueHash(hash));
+    }
+
+    @Test
+    public void buildFullUrlTest() {
+
+        HashMap<String, Object> webSiteRow = new HashMap<String, Object>();
+
+        webSiteRow.put("url", "/2016/01/21/the-49ers-relationship-with-kaepernick-becomes-even more-bizarre");
+        webSiteRow.put("domain", "profootballtalk.nbcsports.com");
+        webSiteRow.put("secure", 0);
+
+        Assert.assertEquals("Crawler Url builder should create valid uri component",
+                "http://profootballtalk.nbcsports.com/2016/01/21/the-49ers-relationship-with-kaepernick-becomes-even more-bizarre", Crawler.buildFullUrl(webSiteRow));
+
+        webSiteRow.put("url", "/106402933672/videos/10153671119698673/?theater");
+        webSiteRow.put("domain", "www.facebook.com");
+        webSiteRow.put("secure", 1);
+
+        Assert.assertEquals("Crawler Url builder shoudl create valid uri component", "https://www.facebook.com/106402933672/videos/10153671119698673/?theater",
+                Crawler.buildFullUrl(webSiteRow));
     }
 }
